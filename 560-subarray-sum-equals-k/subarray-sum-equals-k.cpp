7e317445aa3e3463a11d542f7b1606eeabfd3346
Samim Ahmed <107166960+samim2020065@users.noreply.github.com>
2025-07-08 16:29:24 +0600

    Time: 66 ms (39.29%) | Memory: 54 MB (5.37%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -3,16 +3,13 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         map<int,int>pre_sum;
         int sum=0,cnt=0;
+        pre_sum[0]=1;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            if(sum==k){
-                cnt++;
-            }
-            int rem=sum-k;
-            if(pre_sum.find(rem)!=pre_sum.end()){
-                cnt+=pre_sum[rem];
-            }
-            pre_sum[sum]++;
+            int remove = sum-k;
+            cnt+=pre_sum[remove];
+            pre_sum[sum]+=1;
+            
         }
         return cnt;
     }
