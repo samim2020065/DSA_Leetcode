e70fd230d7b013976397422ee9b3f50582b37a5a
Samim Ahmed <107166960+samim2020065@users.noreply.github.com>
2025-07-24 07:16:41 +0000

    Time: 0 ms (100.00%) | Memory: 9.3 MB (81.19%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int seen[128]={0};
+        int seen_s[128]={0},seen_t[128]={0};
         int n=s.size();
         for(int i=0;i<n;i++){
-        	char a=s[i],b=t[i];
-        	if(!seen[a]){
-        		for(char c:seen){
-        			if(c==b){
-        				return false;
-        			}
-        		}
-        		seen[a]=b;
-        	}
-        	else if(seen[a]!=b){
+        	if(seen_s[s[i]]!=seen_t[t[i]]){
         		return false;
         	}
+        	seen_s[s[i]]=i+1;
+        	seen_t[t[i]]=i+1;
+
         }
         return true;
     }
