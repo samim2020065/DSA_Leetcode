848a2f8c1a3e36f570e408d604b1d8ed5e9e816f
Samim Ahmed <107166960+samim2020065@users.noreply.github.com>
2025-09-06 16:28:09 +0600

    Time: 0 ms (100.00%) | Memory: 12.2 MB (95.71%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=m-1,j=0;
-        while(i>=0&&j<n){
-            if(nums1[i]>nums2[j]){
-               swap(nums1[i],nums2[j]);
-               i--;
-               j++;
-            }
-            else{
-                break;
-            }
-            
-        }
-        // for(auto it:nums1){
-        //     cout<<it<<" ";
-        // }
-        sort(nums1.begin(),nums1.begin()+m);
-        sort(nums2.begin(),nums2.end());
         int x=0;
-        for(int i=m;i<m+n;i++){
+        for(int i=m;i<n+m;i++){
             nums1[i]=nums2[x];
             x++;
         }
         // for(auto it:nums1){
         //     cout<<it<<" ";
         // }
-
+        int gap = (n+m)/2+(n+m)%2;
+        while(gap>0){
+            int left=0,right=gap;
+            while(right<(n+m)){
+                if(nums1[left]>nums1[right]){
+                    swap(nums1[left],nums1[right]);
+                }
+                left++;
+                right++;
+            }
+            if(gap!=1){
+                gap=(gap/2)+(gap%2);
+            }
+            else{
+                gap=(gap/2);
+            }
+            
+        }
+        // for(auto it:nums1){
+        //     cout<<it<<" ";
+        // }
     }
 };
