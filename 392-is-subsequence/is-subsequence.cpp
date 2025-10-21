f2f309a5e3a0110e550c41da64fe86f06415dde8
Samim Ahmed <107166960+samim2020065@users.noreply.github.com>
2025-10-21 23:46:26 +0600

    Time: 0 ms (100.00%) | Memory: 8.7 MB (33.22%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+bool isSubsequence(string s, string t) {
+        int x=0;
+        if(s.size()==0){
+            return true;
+        }
+        for(int i=0;i<t.size();i++){
+            if(t[i]==s[x]){
+                x++;
+            }
+            if(x==s.size()){
+                return true;
+            }
+        }
+        return false;
+    }
+};
