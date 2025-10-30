19e74b4e825db7fe8e134cdfc00a4f7cd71f2bb4
Samim Ahmed <107166960+samim2020065@users.noreply.github.com>
2025-10-30 07:01:53 +0600

    Time: 75 ms (8.00%) | Memory: 37.2 MB (5.70%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -4,13 +4,21 @@ vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>>ans;
         unordered_map<string,vector<string>>mp;
         for(auto it:strs){
-            string cur=it;
-            sort(cur.begin(),cur.end());
-            mp[cur].push_back(it);
+            vector<int>cnt(26,0);
+            for(char c:it){
+                cnt[c-'a']++;
+            }
+            string key="";
+            for(auto x:cnt){
+                key+=to_string(x)+"#";
+            }
+            mp[key].push_back(it);
         }
         for(auto it:mp){
             ans.push_back(it.second);
         }
         return ans;
+        
     }
+
 };
