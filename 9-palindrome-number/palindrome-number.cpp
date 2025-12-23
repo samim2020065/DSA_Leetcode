18c87faf22baff89c7914dafbbb8f04873181974
Samim Ahmed <107166960+samim2020065@users.noreply.github.com>
2025-12-23 22:33:40 +0600

    Time: 1 ms (57.84%) | Memory: 8.7 MB (41.45%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+bool isPalindrome(int x) {
+        if(x<0||((x%10==0)&&(x!=0))){
+            return false;
+        }
+        int reverse=0;
+        while(x>reverse){
+            reverse=reverse*10+(x%10);
+            x/=10;
+        }
+        if(x==reverse||(x==(reverse/10))){
+            return true;
+        }
+        return false;
+    }
+};
