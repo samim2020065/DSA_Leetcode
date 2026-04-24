70bf62e505e1aa45b52306f6c3edb885f6c90023
Samim Ahmed <107166960+samim2020065@users.noreply.github.com>
2026-04-24 11:42:28 +0600

    Time: 10 ms (68.00%) | Memory: 9.2 MB (99.39%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
+    int func(int left,int right,string& s){
+        int mx=0;
+        while(left>=0&&right<s.size()&&s[left]==s[right]){
+            mx=max(mx,right-left+1);
+            left--;
+            right++;
+        }
+        return mx;
+    }
     string longestPalindrome(string s) {
-        int n=s.size();
-        int start=0,mxLen=1;
-        for(int i=1;i<n;i++){
-        	int l=i-1,r=i;
-        	while(l>=0&&r<n&&s[l]==s[r]){
-        		if((r-l+1)>mxLen){
-        			mxLen=r-l+1;
-        			start=l;
-        		}
-        		l--;
-        		r++;
-        	}
-        	l=i-1,r=i+1;
-        	while(l>=0&&r<n&&s[l]==s[r]){
-        		if((r-l+1)>mxLen){
-        			mxLen=r-l+1;
-        			start=l;
-        		}
-        		l--;
-        		r++;
-        	}
+        int mxLen=0,start;
+        for(int i=0;i<s.size();i++){
+            int len1=func(i,i,s);
+            int len2=func(i,i+1,s);
+            int len=max(len1,len2);
+            if(len>mxLen){
+                mxLen=len;
+                start=i-(mxLen-1)/2;
+            }
         }
         return s.substr(start,mxLen);
     }
