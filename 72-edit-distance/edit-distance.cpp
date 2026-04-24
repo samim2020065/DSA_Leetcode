a7803e796483d0879e9110d282d3f8991a3cbff7
Samim Ahmed <107166960+samim2020065@users.noreply.github.com>
2026-04-24 12:19:03 +0600

    Time: 3 ms (91.89%) | Memory: 14.3 MB (17.31%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-int func(int x,int y,string &s1,string &s2,vector<vector<int>>&dp){
-	if(x==0){
-		return y;
-	}
-	if(y==0){
-		return x;
-	}
-	if(dp[x][y]!=-1){
-		return dp[x][y];
-	}
-	if(s1[x-1]==s2[y-1]){
-		return dp[x][y]=func(x-1,y-1,s1,s2,dp);
-	}
-	int del=func(x-1,y,s1,s2,dp);
-	int ins=func(x,y-1,s1,s2,dp);
-	int rep=func(x-1,y-1,s1,s2,dp);
-	return dp[x][y]=1+min({del,ins,rep});
-}
-int minDistance(string word1, string word2) {
-	int n=word1.size(),m=word2.size();
-	vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-	return func(n,m,word1,word2,dp);
-}
+    int func(int x,int y,string&word1,string &word2,vector<vector<int>>&dp){
+        if(x<0){
+            return y+1;
+        }
+        if(y<0){
+            return x+1;
+        }
+        if(dp[x][y]!=-1){
+            return dp[x][y];
+        }
+        if(word1[x]==word2[y]){
+            dp[x][y]=func(x-1,y-1,word1,word2,dp);
+        }
+        else{
+            int del=func(x-1,y,word1,word2,dp);
+            int ins=func(x,y-1,word1,word2,dp);
+            int rep=func(x-1,y-1,word1,word2,dp);
+            dp[x][y]=1+min({del,ins,rep});
+        }
+        return dp[x][y];
+    }
+    int minDistance(string word1, string word2) {
+        int n=word1.size(),m=word2.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return func(n-1,m-1,word1,word2,dp);
+    }
 };
