12827469b9842a4865f21f560df4b2f694f4e81d
Samim Ahmed <107166960+samim2020065@users.noreply.github.com>
2026-04-24 12:32:29 +0600

    Time: 33 ms (12.72%) | Memory: 31.2 MB (17.06%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-int func(int x,int y,int n,int m,vector<vector<char>>&matrix,vector<vector<int>>&dp){
-	if(x>=n||y>=m){
-		return 0;
-	}
-	if(dp[x][y]!=-1){
-		return dp[x][y];
-	}
-	int right=func(x,y+1,n,m,matrix,dp);
-	int down=func(x+1,y,n,m,matrix,dp);
-	int diag=func(x+1,y+1,n,m,matrix,dp);
-	if(matrix[x][y]=='1'){
-		dp[x][y]=1+min({right,down,diag});
-	}
-	else{
-		dp[x][y]=0;
-	}
-	return dp[x][y];
-}
-int maximalSquare(vector<vector<char>>& matrix) {
-	int n=matrix.size(),m=matrix[0].size();
-	vector<vector<int>>dp(n,vector<int>(m,-1));
-	int ans=0;
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
-			ans=max(ans,func(i,j,n,m,matrix,dp));
-		}
-	}
-	return ans*ans;
-}
+    int func(int x,int y,vector<vector<char>>& matrix,vector<vector<int>>& dp){
+        int n=matrix.size(),m=matrix[0].size();
+        if(x>=n||y>=m){
+            return 0;
+        }
+        if(dp[x][y]!=-1){
+            return dp[x][y];
+        }
+        if(matrix[x][y]=='0'){
+            dp[x][y]=0;
+        }
+        else{
+            int right=func(x,y+1,matrix,dp);
+            int down=func(x+1,y,matrix,dp);
+            int diag=func(x+1,y+1,matrix,dp);
+            dp[x][y]=1+min({right,down,diag});
+        }
+        return dp[x][y];
+
+    }
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int n=matrix.size(),m=matrix[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        int mx=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int res=func(i,j,matrix,dp);
+                mx=max(mx,res*res);
+            }
+        }
+        return mx;
+    }
 };
