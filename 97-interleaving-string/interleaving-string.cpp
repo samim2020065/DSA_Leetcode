34e993aa2ff0dd3594014117b8c4e3335fb5eb29
Samim Ahmed <107166960+samim2020065@users.noreply.github.com>
2026-04-24 11:53:16 +0600

    Time: 2 ms (63.49%) | Memory: 9.4 MB (50.77%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-bool isInterleave(string s1, string s2, string s3) {
-	int n=s1.size(),m=s2.size();
-	if((n+m)!=s3.size()){
-		return false;
-	}
-	vector<bool>dp(m+1,false);
-	dp[0]=true;
-	for(int i=1;i<=m;i++){
-		dp[i]=dp[i-1]&&(s2[i-1]==s3[i-1]);
-	}
-	for(int i=1;i<=n;i++){
-		dp[0]=dp[0]&&(s1[i-1]==s3[i-1]);
-		for(int j=1;j<=m;j++){
-			int k=i+j-1;
-			dp[j]=(dp[j-1]&&(s2[j-1]==s3[k]))||(dp[j]&&s1[i-1]==s3[k]);
-		}
-	}
-	return dp[m];
-}
+    bool isInterleave(string s1, string s2, string s3) {
+        if(s3.size()!=s1.size()+s2.size()){
+            return false;
+        }
+        int n=s1.size(),m=s2.size();
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+        dp[0][0]=true;
+        for(int i=1;i<=n;i++){
+            dp[i][0]=dp[i-1][0]&&(s1[i-1]==s3[i-1]);
+        }
+        for(int i=1;i<=m;i++){
+            dp[0][i]=dp[0][i-1]&&(s2[i-1]==s3[i-1]);
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int k=i+j-1;
+                dp[i][j]=(dp[i-1][j]&&s1[i-1]==s3[k])||(dp[i][j-1]&&s2[j-1]==s3[k]);
+            }
+        }
+        return dp[n][m];
+    }
 };
