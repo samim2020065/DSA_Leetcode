a35313933c441d1a5d3631afb5711e698087b650
Samim Ahmed <107166960+samim2020065@users.noreply.github.com>
2026-05-05 23:10:22 +0600

    Time: 27 ms (36.52%) | Memory: 24.1 MB (21.04%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -1,65 +1,40 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        int n=s.size();
         int wordLen=words[0].size();
-        int totalLen=words.size()*wordLen;
+        int total_words=words.size();
+        int totalLen=wordLen*total_words;
+        int n=s.size();
         vector<int>ans;
-        if(n<totalLen){
-            return ans;
-        }
-        unordered_map<string,int>freq,seen;
+        unordered_map<string,int>freq;
         for(auto it:words){
             freq[it]++;
         }
         for(int i=0;i<wordLen;i++){
-            int left=i;
-            int cnt=0;
-            for(int right=i;right+wordLen<=n;right+=wordLen){
-                string word=s.substr(right,wordLen);
-                if(freq.find(word)!=freq.end()){
-                    seen[word]++;
+            unordered_map<string,int>window;
+            int left=i,cnt=0;
+            for(int j=i;j+wordLen<=n;j+=wordLen){
+                string temp=s.substr(j,wordLen);
+                if(freq.count(temp)){
+                    window[temp]++;
                     cnt++;
-                    while(seen[word]>freq[word]){
-                        string leftword=s.substr(left,wordLen);
-                        seen[leftword]--;
+                    while(window[temp]>freq[temp]){
+                        string leftWord=s.substr(left,wordLen);
                         left+=wordLen;
+                        window[leftWord]--;
                         cnt--;
-
                     }
-                    if(cnt==words.size()){
+                    if(cnt==total_words){
                         ans.push_back(left);
-                        string leftword=s.substr(left,wordLen);
-                        seen[leftword]--;
-                        left+=wordLen;
-                        cnt--;
                     }
                 }
                 else{
-                    seen.clear();
+                    window.clear();
                     cnt=0;
-                    left=right+wordLen;
+                    left=j+wordLen;
                 }
             }
-            seen.clear();
         }
         return ans;
-    }
-bool isValidSudoku(vector<vector<char>>& board) {
-        bool rows[9][9]={false},cols[9][9]={false},boxes[9][9]={false};
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                if(board[i][j]!='.'){
-                    int num=board[i][j]-'1';
-                    int d=(i/3)*3+(j/3);
-                    if(rows[i][num]||cols[j][num]||boxes[d][num]){
-                        return false;
-                    }
-                    rows[i][num]=cols[j][num]=boxes[d][num]=true;
-                }
-                
-            }
-        }
-        return true;
     }
 };
